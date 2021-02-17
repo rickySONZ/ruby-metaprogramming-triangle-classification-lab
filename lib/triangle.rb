b0a4c444ce301abed8a503a9c695455169107f67
Ryan Erricson <ryanerricson@gmail.com>
2021-02-17 15:31:03 -0500

    commit tree 19fa1c3fd9523b730ef0a2374b6714d2bec53efb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require "pry"
+
 class Triangle
   # write code here
+
+
+  attr_accessor :sideA, :sideB, :sideC
+
+
+  def initialize (sideA, sideB, sideC)
+    @sideA = sideA
+    @sideB = sideB
+    @sideC = sideC
+  end
+
+def kind
+  if sideA + sideB <= sideC || sideB + sideC <= sideA || sideA + sideC <= sideB
+    raise TriangleError
+  elsif sideA <= 0 || sideB <= 0 || sideC <= 0
+    raise TriangleError
+  elsif sideA == sideB && sideA == sideC
+    return :equilateral
+  elsif sideA != sideB && sideA != sideC && sideC != sideB
+    return :scalene
+  else
+    return :isosceles
+
+  end
+  
+  
+
+end
+
+class TriangleError < StandardError
+  # triangle error code
+
+end
 end
